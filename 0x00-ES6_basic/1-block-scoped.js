bc4350d027933d9fa4c7d946abf6cec48cc2d68e
aelbouaa <hbouaami@gmail.com>
2024-05-27 21:14:41 +0100

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,8 +6,8 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
     if (task && task2) {
-        return [task, task2];
-      }
+      return [task, task2];
+    }
   }
   return [task, task2];
 }
