e0655c405e00bf9a8e9e3246a327ec2961bc5c28
aelbouaa <hbouaami@gmail.com>
2024-05-30 21:04:09 +0100

    task 6 corrct syntax

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.settled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      const arr = [];
-      for (const itm of values) {
-        arr.push({ status: itm.status, value: itm.value || itm.reason });
-      }
-      return arr;
-    });
+  return Promise.allsettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const itm of values) {
+      arr.push({ status: itm.status, value: itm.value || itm.reason });
+    }
+    return arr;
+  });
 }
