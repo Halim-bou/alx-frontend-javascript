c6b6ce37e20dd55370510aef3a99bb950d323788
aelbouaa <hbouaami@gmail.com>
2024-05-31 00:34:49 +0100

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,10 +3,11 @@ export default function guardrail(mathFunction) {
   let result;
   try {
     result = mathFunction();
-  } catch (e) {
-    result = `Error: ${e.message}`;
+  } catch (error) {
+    result = `Error: ${error.message}`;
   }
   queue.push(result);
   queue.push('Guardrail was processed');
+
   return queue;
 }
