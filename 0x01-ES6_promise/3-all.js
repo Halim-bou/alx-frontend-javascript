2a9a071b03c65fb9cf0f49ae90738c691eeacce3
aelbouaa <hbouaami@gmail.com>
2024-05-30 20:24:29 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((resolve) => {
       console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
     })
