62908df04f2ce5c11a81556ed7327e5025127cad
aelbouaa <hbouaami@gmail.com>
2024-05-30 20:35:49 +0100

    task 4 syntax error

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve(
     {
-      firstNamr: `${firstName}`,
+      firstName: `${firstName}`,
       lastName: `${lastName}`,
     },
   );
